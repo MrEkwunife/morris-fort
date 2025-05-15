35af036c98b9861ce96b9ad24d3c3205e6ecfadd
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2025-05-15 10:35:33 +0100

    feat: built image composition component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import ImageComposition from "@/components/ui/imageComposition";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <ImageComposition />
     </div>
   );
 }
